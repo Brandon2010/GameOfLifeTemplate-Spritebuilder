215142d2329bdc6844b54bed2aacaaaf64823c8c
Brandon2010 <wxbrandon2010@gmail.com>
2015-03-01 19:40:16 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -154,7 +154,7 @@ static const int GRID_COLUMNS = 10;
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = YES;
                 
-            } else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4) {
+            } else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors > 4) {
                 currentCreature.isAlive = NO;
             }
             
